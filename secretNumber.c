08f85ac089b1b609b77146d55fd98c9c7fb95553
Nguyen Kelly (HES) <kelly.nguyen@hepia-ws-8775.hes.adhes.hesge.ch>
2020-09-16 15:36:21 +0200

    nul

---
+++ secretNumber.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+int main() {
+  srand(time(NULL));
+  int rnd = rand();
+  int rnd2 = 0;
+  int avgTry = 0;
+  int maxNb = 0;
+  int userNb = 0;
+  int nbTry = 0;
+
+  printf("Entrer un nombre maximum : ");
+  scanf("%d", &maxNb);
+
+  int secretNb = rnd % maxNb + 1;
+  rnd2 = secretNb;
+
+  while (rnd2 != 0) {
+    rnd2 /= 2;
+    avgTry++;
+  }
+
+  do {
+    nbTry++;
+    printf("Entrer un nombre : ");
+    scanf("%d", &userNb);
+    if (userNb == secretNb) {
+      printf("Bravo vous avez gagné ! Vous avez fait %d essais \n", nbTry);
+      printf("Le nombre d'essaie optimale est de %d\n", avgTry);
+      break;
+    }else{
+      if (userNb < secretNb && userNb > 0) {
+        printf("Plus grand \n");
+      }else{
+        printf("Plus petit \n");
+      }
+    }
+  }
+  while (userNb != secretNb);
+}
